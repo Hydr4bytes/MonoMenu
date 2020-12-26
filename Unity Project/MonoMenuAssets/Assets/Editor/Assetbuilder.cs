c1da6c45fd051fe2196ecdd6d525d1e73693ffa9
L4rs <45175455+Hydr4bytes@users.noreply.github.com>
2020-12-26 23:18:44 +0100

    Added Unity Project

---
+++ Unity Project/MonoMenuAssets/Assets/Editor/Assetbuilder.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class BuildAssetBundles
+{
+    [MenuItem("Build/Build AssetBundles")]
+    static void BuildAllAssetBundles()
+    {
+        string outputFolder = "Assets/__Bundles";
+
+        //Check if __Bundles folder exist
+        if (!AssetDatabase.IsValidFolder(outputFolder))
+        {
+            Debug.Log("Folder '__Bundles' does not exist, creating new folder");
+
+            AssetDatabase.CreateFolder("Assets", "__Bundles");
+        }
+
+        BuildPipeline.BuildAssetBundles(outputFolder, BuildAssetBundleOptions.ChunkBasedCompression, EditorUserBuildSettings.activeBuildTarget);
+    }
+}
